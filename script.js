e627b052371fe3a1d5fd9a59f16dd96ef0d4fdd7
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-10 14:46:49 +0200

    create algorithm for prime numbers

---
+++ script.js
@@ -0,0 +1,15 @@
+let n = 100; // put any natural n > 1, 100 is placeholder
+
+// first loop for evaluating every natural from 2 up to n
+// second loop to divide every natural by numbers from 2 up to n
+
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j <= i; j++) {
+    if (i % j === 0 && i / j === 1) {
+      console.log(`${i} is prime number`);
+    }
+    if (i % j === 0 && i / j > 1) {
+      break;
+    }
+  }
+}
